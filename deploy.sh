cc7f974d7ad5d73c0892eed74b2a311a6ae90d7b
lagleki <gleki.is.my.name@gmail.com>
2021-01-16 08:47:18 -0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 rm -rf project/build
-git add . ; git commit -m "gix" ; git push
 git init
 git remote add origin https://github.com/lagleki/melbi-zei-lojban.git
 git rm -r --cached .
 git add .
-git commit -m "first commit"
-git push -f
+git commit -m "fix"
 git push --set-upstream origin master -f
 docker exec -it snanu bash
